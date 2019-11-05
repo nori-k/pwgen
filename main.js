2bd615ab19a00245ffe899ba252bbbac0338a030
nori-k <norikatsu.kamiya@gmail.com>
2019-11-05 16:18:44 +0900

    remove comments

--- main.js
+++ main.js
@@ -3,40 +3,34 @@ const { app, BrowserWindow } = require("electron");
 let win;
 
 function createWindow() {
-  //ウインドウの作成
   win = new BrowserWindow({
     width: 400,
     height: 450,
     webPreferences: {
-      nodeIntegration: true //Electron6から必要らしい
+      nodeIntegration: true
     }
   });
 
-  //ウインドウに表示する内容
   win.loadFile("src/index.html");
 
-  //デバッグ画面表示
+  //if debug window needed, remove commentout line below
   // win.webContents.openDevTools()
 
-  //このウインドウが閉じられたときの処理
   win.on("closed", () => {
     win = null;
   });
 }
 
-//アプリが初期化されたとき（起動されたとき）
 app.on("ready", () => {
   createWindow();
 });
 
-//全ウインドウが閉じられたとき
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-//アクティブになったとき（MacだとDockがクリックされたとき）
 app.on("activate", () => {
   if (win === null) {
     createWindow();
